f9440a3610ffe61bcafaea7c9a0a2ab398cad9ba
maciej.zelias <maciej.zelias@besidethepark.com>
2023-06-21 00:11:04 +0200

    Upgrade navigation Create initial layout for job-offers page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,9 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navigation from "./components/fragments/Navigation";
+import MainContent from "./components/fragments/MainContent";
+import NotFound from "./components/other/NotFound";
+import JobOfferList from "./components/job-offers/JobOffersList";
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -7,6 +11,12 @@ function App() {
   return (
     <>
       <Navigation />
+      <Routes>
+        <Route path="/" element={<MainContent />} />
+        <Route path="/applications" element={<NotFound />} />
+        <Route path="/job-offers" element={<JobOfferList />} />
+        <Route path="/recruitments" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
--- client/src/components/fragments/Navigation.tsx
+++ client/src/components/fragments/Navigation.tsx
@@ -1,32 +1,39 @@
 import { Link } from "react-router-dom";
+import logoImage from "../../assets/logo.png";
 
 export default function Navigation() {
   return (
-    <nav className="bg-gray-800 w-full">
+    <nav className="bg-gray-800 m-6">
       <div className="container mx-auto px-4">
-        <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
-            <Link to={"/"} className="text-white text-lg font-bold">
-              Logo
+        <div className="flex items-center justify-between">
+          <div className="flex flex-row gap-5 items-center">
+            <Link to={"/"} className="text-white text-lg font-bold h-16">
+              <img src={logoImage} className="w-full h-full" />
             </Link>
+            <h1 className="font-sans text-2xl font-bold">RecruITer</h1>
           </div>
-          <div className="hidden md:block">
-            <div className="ml-10 flex items-baseline space-x-4">
-              <a
-                className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                href="/">
+          <div className="hidden md:block w-3/4">
+            <div className="flex space-x-4">
+              <Link
+                className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-base font-medium"
+                to="/">
                 Home
-              </a>
-              <a
-                className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                href="/about">
-                About
-              </a>
-              <a
-                className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                href="/contact">
-                Contact
-              </a>
+              </Link>
+              <Link
+                className="text-gray-300 hover:text-white px-3 py-2 rounded-md ext-base font-medium"
+                to="/applications">
+                Applications
+              </Link>
+              <Link
+                className="text-gray-300 hover:text-white px-3 py-2 rounded-md ext-base font-medium"
+                to="/job-offers">
+                Job offers
+              </Link>
+              <Link
+                className="text-gray-300 hover:text-white px-3 py-2 rounded-md ext-base font-medium"
+                to="/recruitments">
+                Recruitments
+              </Link>
             </div>
           </div>
         </div>
