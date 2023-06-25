27bb136eaa27eb98288ca0619be0592852a249ff
maciej.zelias <maciej.zelias@besidethepark.com>
2023-06-25 17:14:05 +0200

    Add initial job offer form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,8 @@ import Navigation from "./components/fragments/Navigation";
 import MainContent from "./components/fragments/MainContent";
 import NotFound from "./components/other/NotFound";
 import JobOfferList from "./components/job-offers/JobOffersList";
+import JobOfferPreview from "./components/job-offers/JobOfferPreview";
+import JobOfferForm from "./components/job-offers/JobOfferForm";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<MainContent />} />
         <Route path="/applications" element={<NotFound />} />
         <Route path="/job-offers" element={<JobOfferList />} />
+        <Route path="/job-offers/new" element={<JobOfferForm />} />
+        <Route path="/job-offers/:id" element={<JobOfferPreview />} />
         <Route path="/recruitments" element={<NotFound />} />
       </Routes>
     </>
