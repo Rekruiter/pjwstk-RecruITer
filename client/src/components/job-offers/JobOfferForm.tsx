ecc2530295a96c59f129a9c462addb42905c5068
Maciej Zelias <s22482@pjwstk.edu.pl>
2023-06-26 21:26:14 +0200

    Add validation Connect to ednpoints

--- client/src/components/job-offers/JobOfferForm.tsx
+++ client/src/components/job-offers/JobOfferForm.tsx
@@ -1,16 +1,61 @@
 import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 interface QuestionForm {
   question: string;
 }
 
+const validationSchema = Yup.object().shape({
+  jobName: Yup.string().required("Job Name is required"),
+  requirements: Yup.string().required("Requirements are required"),
+  salary: Yup.number().required("Salary is required"),
+});
+
 export default function JobOfferForm() {
   const navigate = useNavigate();
-  const [jobName, setJobName] = useState("");
-  const [location, setLocation] = useState("");
-  const [salary, setSalary] = useState("");
+  const [error, setError] = useState<string>("");
   const [questions, setQuestions] = useState<QuestionForm[]>([]);
+
+  const formik = useFormik({
+    initialValues: {
+      jobName: "",
+      requirements: "",
+      salary: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (values, { resetForm }) => {
+      const response = await fetch(
+        "https://recruiter-endpoints.azurewebsites.net/api/jobOffers",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            IdCompany: "1",
+            Salary: values.salary,
+            Description: values.jobName,
+            Requirments: values.requirements,
+            DateExpires: "2023-07-25T10:00:00",
+          }),
+        }
+      );
+
+      if (response.status === 201) {
+        resetForm();
+        navigate("/job-offers");
+      } else {
+        setError(
+          "Something went wrong : " +
+            response.status +
+            " " +
+            response.statusText
+        );
+      }
+    },
+  });
 
   const handleQuestionChange = (index: number, value: string) => {
     const newQuestions = [...questions];
@@ -28,56 +73,65 @@ export default function JobOfferForm() {
     setQuestions(newQuestions);
   };
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-
-    // Form submission logic goes here
-    // You can access the form data like jobName, location, salary, and questions
-
-    // Reset form fields after submission
-    setJobName("");
-    setLocation("");
-    setSalary("");
-    setQuestions([]);
-    navigate("/job-offers");
-  };
-
   return (
     <div className="mx-5 flex-1">
       <div className="bg-gray-700 rounded-lg flex flex-col">
         <div className="m-5 bg-blue-950 rounded-lg flex flex-col justify-center items-center">
-          <form onSubmit={handleSubmit} className="bg-teal-800 m-5">
-            <div className="m-5 flex justify-between gap-5">
-              <label htmlFor="jobName">Job Offer Name:</label>
-              <input
-                className="bg-white rounded-lg text-black"
-                type="text"
-                id="jobName"
-                value={jobName}
-                onChange={(e) => setJobName(e.target.value)}
-              />
+          <form
+            onSubmit={formik.handleSubmit}
+            className="bg-teal-800 rounded-lg m-5">
+            <div>
+              <div className="m-5 flex justify-between gap-5">
+                <label htmlFor="jobName">Job Offer Name:</label>
+                <input
+                  className="bg-white rounded-lg text-black"
+                  type="text"
+                  id="jobName"
+                  value={formik.values.jobName}
+                  onChange={formik.handleChange}
+                />
+              </div>
+              {formik.errors.jobName && formik.touched.jobName && (
+                <div className="text-center text-red-600 font-bold">
+                  {formik.errors.jobName}
+                </div>
+              )}
             </div>
 
-            <div className="m-5 flex justify-between gap-5">
-              <label htmlFor="location">Location:</label>
-              <input
-                className="bg-white rounded-lg text-black"
-                type="text"
-                id="location"
-                value={location}
-                onChange={(e) => setLocation(e.target.value)}
-              />
+            <div>
+              <div className="m-5 flex justify-between gap-5">
+                <label htmlFor="location">Requirments:</label>
+                <input
+                  className="bg-white rounded-lg text-black"
+                  type="text"
+                  id="requirements"
+                  value={formik.values.requirements}
+                  onChange={formik.handleChange}
+                />
+              </div>
+              {formik.errors.requirements && formik.touched.requirements && (
+                <div className="text-center text-red-600 font-bold">
+                  {formik.errors.requirements}
+                </div>
+              )}
             </div>
 
-            <div className="m-5 flex justify-between gap-5">
-              <label htmlFor="salary">Salary:</label>
-              <input
-                className="bg-white rounded-lg text-black"
-                type="text"
-                id="salary"
-                value={salary}
-                onChange={(e) => setSalary(e.target.value)}
-              />
+            <div>
+              <div className="m-5 flex justify-between gap-5">
+                <label htmlFor="salary">Salary:</label>
+                <input
+                  className="bg-white rounded-lg text-black"
+                  type="number"
+                  id="salary"
+                  value={formik.values.salary}
+                  onChange={formik.handleChange}
+                />
+              </div>
+              {formik.errors.salary && formik.touched.salary && (
+                <div className="text-center text-red-600 font-bold">
+                  {formik.errors.salary}
+                </div>
+              )}
             </div>
 
             <div className="m-5 flex flex-col justify-between gap-5">
@@ -105,6 +159,7 @@ export default function JobOfferForm() {
                 Add Question
               </button>
             </div>
+            {error}
             <div className="text-center">
               <button
                 className="rounded-lg shadow-lg bg-teal-300 m-5 text-black"
