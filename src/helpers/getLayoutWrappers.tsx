e34420b175477ed849503bbfc9713ec19a594ce5
Maciej Zelias <s22482@pjwstk.edu.pl>
2023-11-04 18:01:52 +0100

    Divide layout wrappers logic

---
+++ src/helpers/getLayoutWrappers.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout/Layout';
+
+export const wrapInLayout = (element: JSX.Element, withoutMargin?: boolean) => (
+  <Layout withoutMargin={withoutMargin}>{element}</Layout>
+);
+
+export const wrapInPanelLayout = (element: JSX.Element, withoutMargin?: boolean) => (
+  <Layout withoutMargin={withoutMargin} panel>
+    {element}
+  </Layout>
+);
