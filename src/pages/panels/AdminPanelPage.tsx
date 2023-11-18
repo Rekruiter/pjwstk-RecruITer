cecb7c14a433bca1215d3d575b503933d4b55c88
maciej.zelias <maciej.zelias@besidethepark.com>
2023-11-18 20:55:20 +0100

    Fix build error

--- src/pages/panels/AdminPanelPage.tsx
+++ src/pages/panels/AdminPanelPage.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const AdminPanelPage = () => {
   return <div>AdminPanelPage</div>;
 };
--- src/pages/panels/RecruiterPanelPage.tsx
+++ src/pages/panels/RecruiterPanelPage.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const RecruiterPanelPage = () => {
   return <div>RecruiterPanelPage</div>;
 };
